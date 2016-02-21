b1553b6da2abb81ec549c6a2c01f6563e9401fc7
Steph Monette <steph@muledesign.com>
2016-02-21 13:13:28 -0800

    ITS ALIVEEEEEEE! Also some basic styling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express'),
+    db = require('./models/index.js'),
+    bodyParser = require('body-parser'),
     app = express();
 
 var http = require('http').createServer(app);
@@ -6,10 +8,33 @@ var http = require('http').createServer(app);
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}) );
+
 
 app.get('/', function(req,res){
-  res.render('index');
+  db.movie.findAll()
+  .then(function(movies){
+    res.render('index', {
+      movies: movies, 
+
+      message:''
+    })
+  })
 });
+
+app.post('/create', function(req,res){
+  // have to call my create new user functions
+  db.movie.createNewMovie(req.body.title, req.body.review,req.body.image, req.body.rating,
+    function(err){
+      console.log('NOO')
+    },
+    function(success){
+      res.redirect('/');
+    }
+  );
+
+});
+
 
 
 
---
+++ models/movie.js
@@ -0,0 +1,55 @@
+module.exports = function (sequelize, DataTypes){
+
+    var Movie = sequelize.define('movie',{
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      review: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true
+      },
+      image: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true
+      },
+      rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true
+      }     
+     }, //close the first opening bracket after ('movie',
+
+    {
+    classMethods: {
+      createNewMovie:function(title,review,image,rating,err,success ) {
+        Movie.create({
+          title: title,
+          review: review,
+          image: image,
+          rating:rating
+        }).error(function(error) {
+          console.log(error);
+        }).then(function(movie) {
+          success({message: 'Movie added!'});
+        });
+      } // close createNewMovie
+
+    } // close classMethods
+
+  } //close classMethods outer
+
+  )
+  return Movie;
+
+};
+
+
+
+
+
+
+
